c55e833e808269eb6e31e37d77525196e4d81a54
Younes Lahouiti <loller763@gmail.com>
2022-09-06 20:11:02 +0200

    Petit changement pour le darkmode

--- dark-mode.js
+++ dark-mode.js
@@ -1,18 +1,30 @@
 const themeTogglerButton = document.querySelector(".theme__toggler-label");
 const body = document.body;
-
-const moonIcon = document.querySelector(".fa-moon");
-const sonIcon = document.querySelector(".fa-sun");
+const checkboxToggler = document.querySelector("#toggler");
 
 themeTogglerButton.addEventListener("click", switchTheme);
 
-function switchTheme(e) {
-  console.log(e.target);
+let usesDarkMode = false;
 
+function switchTheme(e) {
+  if (usesDarkMode) {
+    body.classList.add("light-theme");
+  } else {
+    body.classList.add("dark-theme");
+  }
+
+  console.log(body.classList);
+}
+
+function checkTheBoxIfDarkMode() {
   if (
     window.matchMedia("(prefers-color-scheme: dark)") &&
     window.matchMedia("(prefers-color-scheme: dark)").matches
   ) {
     console.log("Users uses dark mode");
+    checkboxToggler.checked = true;
+    usesDarkMode = true;
   }
 }
+
+checkTheBoxIfDarkMode();
